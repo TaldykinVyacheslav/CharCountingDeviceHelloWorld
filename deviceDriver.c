374bc8ead3dac185cc4f10c6ec32aa07e933b833
Slava <zegoline@gmail.com>
2014-04-09 00:41:40 +0300

    Added operation procedures

--- deviceDriver.c
+++ deviceDriver.c
@@ -21,3 +21,39 @@ void cleanup_module(void)
 {
     unregister_chrdev(89, "myDev");
 }
+
+
+static int dev_open(struct inode *inod, struct file *fil)
+{
+    return 0;
+}
+
+static ssize_t dev_read(struct file *filp, char *buff, size_t len, loff_t *off)
+{
+    return count;
+}
+
+static ssize_t dev_write(struct file *filp, const char *buff, size_t len, loff_t *off)
+{
+    clock_t ct0, ct1;
+    struct tms tms0, tms1;
+    sscanf(buff, "%d", waitTime);
+    if (ct0 = times (&tms0)) == -1)
+	perror("times");
+    while (1)
+    {
+	if (ct1 = times (&tms11)) == -1)
+	    perror("times");
+	if (ct1 - ct0 >= waitTime) {
+	    ct0 = ct1;
+            printk(KERN_ALERT "Hello!\n");
+        }
+    }
+    return len;
+}
+
+static int dev_rls(struct inode *inod, struct file *fil)
+{
+    printk(KERN_ALERT "Device closed\n");
+    return 0;
+}
